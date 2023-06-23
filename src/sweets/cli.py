71496408e2be4aac7ac230d26788a91a1f3fae9a
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-06-23 13:23:54 -0700

    start breaking out CLI more modularly

--- src/sweets/cli.py
+++ src/sweets/cli.py
@@ -231,3 +231,31 @@ def main(args=None):
     args = _get_cli_args()
     func = args.pop("func")
     func(args)
+
+
+# import argparse
+
+# import dolphin._cli_unwrap
+# import dolphin.workflows._cli_config
+# import dolphin.workflows._cli_run
+# from dolphin import __version__
+
+
+# def main(args=None):
+#     """Top-level command line interface to the workflows."""
+#     parser = argparse.ArgumentParser(
+#         prog=__package__,
+#         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+#     )
+#     subparser = parser.add_subparsers(title="subcommands")
+#     parser.add_argument("--version", action="version", version=__version__)
+
+#     # Adds the subcommand to the top-level parser
+#     dolphin.workflows._cli_run.get_parser(subparser, "run")
+#     dolphin.workflows._cli_config.get_parser(subparser, "config")
+#     dolphin._cli_unwrap.get_parser(subparser, "unwrap")
+#     parsed_args = parser.parse_args(args=args)
+
+#     arg_dict = vars(parsed_args)
+#     run_func = arg_dict.pop("run_func")
+#     run_func(**arg_dict)
